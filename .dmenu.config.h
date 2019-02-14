897a005efcddb9b1a329b147350af0d6a9ea4120
Simon Hauser <Simon.Hauser.XD@live.de>
2019-02-14 18:25:20 +0100

    Updated askpass

--- .dmenu.config.h
+++ .dmenu.config.h
@@ -1,6 +1,9 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
+// Patches
+// https://tools.suckless.org/dmenu/patches/password/dmenu-password-4.7.diff
+
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
