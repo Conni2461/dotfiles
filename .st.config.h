bc42aeea31831d79955826d6bc2c063dec880376
Simon Hauser <Simon.Hauser.XD@live.de>
2019-03-03 00:09:35 +0100

    Updated st to 0.8.2 and added script to build suckless software on save with vim

--- .st.config.h
+++ .st.config.h
@@ -205,7 +205,6 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ TERMMOD,              XK_I,           iso14755,       {.i =  0} },
 	{ ShiftMask,		XK_Page_Up,	kscrollup,	{.i = -1} },
 	{ ShiftMask,		XK_Page_Down,	kscrolldown,	{.i = -1} },
 	{ MODKEY,		XK_Page_Up,	kscrollup,	{.i = -1} },
--- .vimrc
+++ .vimrc
@@ -78,6 +78,7 @@
 	autocmd BufWritePre * %s/\s\+$//e
 
 " Post Save Commands
+	autocmd BufWritePost * !sucklessbuild %:p
 	autocmd BufWritePost * silent! execute "!notify-send 'File <afile> saved'" | redraw!
 	autocmd BufWritePost * silent! execute "!syncfile %:p" | redraw!
 
