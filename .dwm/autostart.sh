bc12ba0cb65e69e2ed592ec5864ae36857712a0b
Simon Hauser <Simon.Hauser.XD@live.de>
2019-08-01 22:13:14 +0200

    Added calnotify and gputop. Also some other changes

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -4,11 +4,12 @@ xinput --set-prop "Microsoft Surface Keyboard Touchpad" "libinput Accel Speed" 0
 dunst &
 compton &
 nitrogen --restore &
-xautolock -time 10 -locker 'lock' -notify 30 -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'" &
-clipmenud &
+pgrep clipmenud >/dev/null|| clipmenud &
 
 syncshared >/dev/null &
 newsup >/dev/null &
 mailsync >/dev/null &
 
 dwmbar &
+
+calnotify 30
