00d57ade10efca1ab3315f0618905670a5a1dc65
Simon Hauser <Simon-Hauser@outlook.de>
2020-02-14 19:02:25 +0100

    Switched from nitrogen to feh

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -4,7 +4,7 @@ g810-led -a ff0000 &
 pgrep dunst >/dev/null || dunst &
 pgrep compton >/dev/null || compton &
 fixdisplay &
-nitrogen --restore &
+feh --bg-fill ~/img/2560x1440.jpg --bg-fill ~/img/1920x1080.jpg
 pgrep clipmenud >/dev/null|| clipmenud &
 
 calsync >/dev/null &
