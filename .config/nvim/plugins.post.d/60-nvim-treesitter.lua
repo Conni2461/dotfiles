c6991cccacfa8697978cb35a2ad3bb559bb07826
Simon Hauser <Simon-Hauser@outlook.de>
2020-06-29 21:04:56 +0200

    Fixes formatting and adds more languages

--- .config/nvim/plugins.post.d/60-nvim-treesitter.lua
+++ .config/nvim/plugins.post.d/60-nvim-treesitter.lua
@@ -3,17 +3,38 @@ local ts = require'nvim-treesitter.configs'
 ts.setup {
 	highlight = {
 		enable = true,                    -- false will disable the whole extension
-		-- disable = { 'c', 'rust' },        -- list of language that will be disabled
+		-- disable = { 'c', 'rust' },     -- list of language that will be disabled
 	},
 	incremental_selection = {
 		enable = true,
-		disable = { 'cpp', 'lua' },
+		-- disable = { 'cpp', 'lua' },    -- list of language that will be disabled
 		keymaps = {                       -- mappings for incremental selection (visual mappings)
-			init_selection = 'gnn',         -- maps in normal mode to init the node/scope selection
-			node_incremental = "grn",       -- increment to the upper named parent
-			scope_incremental = "grc",      -- increment to the upper scope (as defined in locals.scm)
-			node_decremental = "grm",      -- decrement to the previous node
+			init_selection = 'gnn',       -- maps in normal mode to init the node/scope selection
+			node_incremental = "grn",     -- increment to the upper named parent
+			scope_incremental = "grc",    -- increment to the upper scope (as defined in locals.scm)
+			node_decremental = "grm",     -- decrement to the previous node
 		}
 	},
-	ensure_installed = { 'bash', 'c', 'cpp', 'css', 'go', 'html', 'java', 'javascript', 'json', 'lua', 'python', 'ruby', 'rust', 'typescript' } -- one of 'all', 'language', or a list of languages
+	ensure_installed = { -- one of 'all', 'language' or a list of languages
+		'bash',
+		'c',
+		'cpp',
+		'css',
+		'c_sharp',
+		'go',
+		'html',
+		'java',
+		'javascript',
+		'json',
+		'lua',
+		'markdown',
+		'python',
+		'regex',
+		'ruby',
+		'rust',
+		'scala',
+		'toml',
+		'typescript',
+		'yaml'
+	}
 }
