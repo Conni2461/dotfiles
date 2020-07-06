9070299868cb6c9cd361de44b746de16855d76a5
Simon Hauser <Simon-Hauser@outlook.de>
2020-07-06 14:41:04 +0200

    Fixes expire key issue. Script stops sending two notification when streamer changes game. Now only one with better message

--- bin/croncmds/twitch-notify.py
+++ bin/croncmds/twitch-notify.py
@@ -10,6 +10,7 @@ import configparser
 import requests
 import notify2
 
+
 def sendmessage(message):
     '''
     Send notification with notify-send
@@ -18,6 +19,7 @@ def sendmessage(message):
     if not notify_off:
         notify2.Notification("Twitch", message).show()
 
+
 def log(message):
     '''
     Log message to stderr and exits script
@@ -25,13 +27,14 @@ def log(message):
     print(message, file=sys.stderr)
     sys.exit()
 
+
 def getNewOAuthToken(client):
     '''
     Prints message on how to receive a new OAuth token.
     '''
     print()
     print("To get a custom OAuth Token run:")
-    print("curl " + 'https://id.twitch.tv/oauth2/authorize?response_type=token&client_id={}&redirect_uri=http://localhost'.format(client) + " > output.html")
+    print("curl 'https://id.twitch.tv/oauth2/authorize?response_type=token&client_id={}&redirect_uri=http://localhost' > output.html".format(client))
     print("Open the html file with your browser and log into twitch. The next page that opens contains the token.")
     print("Paste the new token in the config file.")
 
@@ -67,23 +70,25 @@ user_id = defaultConfig.get("User-ID")
 client = defaultConfig.get("Client-ID")
 token = defaultConfig.get("Access-token")
 
-if user_id == None:
+if user_id is None:
     log("User id not found")
 
-if client == None:
+if client is None:
     log("Client id not found")
 
-if token == None:
+if token is None:
     print("No token found.")
     getNewOAuthToken(client)
     sys.exit()
 
-headers_val = { 'Authorization': 'OAuth {}'.format(token) }
-headers = { 'Client-ID': '{}'.format(client), 'Authorization': 'Bearer {}'.format(token) }
-val = requests.get('https://id.twitch.tv/oauth2/validate', headers=headers_val).json()
-print(val)
-exp_in = int(val["expires_in"])
-if exp_in == 0:
+headers_val = {'Authorization': 'OAuth {}'.format(token)}
+headers = {
+        'Client-ID': '{}'.format(client),
+        'Authorization': 'Bearer {}'.format(token)}
+val_res = requests.get('https://id.twitch.tv/oauth2/validate',
+                       headers=headers_val)
+print(val_res.json())
+if val_res.status_code == 401:
     print("Your token is expired.")
     getNewOAuthToken(client)
     sys.exit()
@@ -103,7 +108,6 @@ except Exception:
 followed = []
 followrequest = apipage + "users/follows?from_id=%s" % user_id
 data = requests.get(followrequest, headers=headers).json()
-print(data)
 
 # Collect all follows.
 # So this will send requests until there are all followers fetched
@@ -117,7 +121,7 @@ while len(data["data"]) != 0:
     data = requests.get(followr, headers=headers).json()
 
 game_cache = {}
-output = set()
+output = {}
 
 for j in range(math.ceil(len(followed) / 100)):
     # Get Stream info
@@ -150,33 +154,51 @@ for j in range(math.ceil(len(followed) / 100)):
                 channel_game = game_json["data"][0]["name"]
                 game_cache[game_id] = channel_game
 
-        # Build output string
-        result = "<b>" + channel_name + "</b>"
-        result += " is <b>LIVE</b> playing <b>" + channel_game + "</b>"
-        output.add(result)
+        output[channel_name] = channel_game
 
 # Load oldData
-prev_output = set()
+prev_output = {}
 try:
     with open(filelocation, "r") as f:
         for line in f:
             if line != '\n' or line != '':
-                prev_output.add(line.replace('\n', ''))
+                c = line.replace('\n', '').split(';')[0]
+                g = line.replace('\n', '').split(';')[1]
+                prev_output[c] = g
 except Exception:
     print('File not created yet')
 
-# Return new livestreams
-went_live = output - prev_output
-went_offline = prev_output - output
+went_live = set(output.items()) - set(prev_output.items())
+went_offline = set(prev_output.items()) - set(output.items())
 
 if went_live or went_offline:
+    # Search for game change
+    channels_l = [x[0] for x in went_live]
+    channels_o = [x[0] for x in went_offline]
+    changed_game = set(channels_l) & set(channels_o)
+
+    # Remove all changed_game's from went_live and went_offline
+    for g in changed_game:
+        for x in went_live:
+            if x[0] == g:
+                went_live.remove(x)
+                break
+        for x in went_offline:
+            if x[0] == g:
+                went_offline.remove(x)
+                break
+
     # Send notifications
     for line in iter(went_live):
-        sendmessage(line)
+        sendmessage("<b>{}</b> is <b>LIVE</b> playing <b>{}</b>"
+                    .format(line[0], line[1]))
     for line in iter(went_offline):
-        sendmessage(line.split("LIVE", 1)[0] + "NO LONGER LIVE")
+        sendmessage("<b>{}</b> is <b>NO LONGER LIVE</b>".format(line[0]))
+    for line in iter(changed_game):
+        sendmessage("<b>{}</b> changed game and is now playing <b>{}</b>"
+                    .format(line, output[line]))
 
     # Update file
     with open(filelocation, "w") as f:
-        for item in output:
-            f.write("%s\n" % item)
+        for k, v in output.items():
+            f.write("{};{}\n".format(k, v))
