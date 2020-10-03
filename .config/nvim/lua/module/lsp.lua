ca0147d35d694a46ac499a94db8ffbf86f8969d6
Simon Hauser <Simon-Hauser@outlook.de>
2020-10-03 08:53:17 +0200

    Changes indent for lua

--- .config/nvim/lua/module/lsp.lua
+++ .config/nvim/lua/module/lsp.lua
@@ -2,135 +2,134 @@ local nvim_lsp = require'nvim_lsp'
 local util     = require'nvim_lsp/util'
 
 local chain_complete_list = {
-	default = {
-		{complete_items = {'lsp', 'snippet'}},
-		{complete_items = {'path'}, triggered_only = {'./', '/'}},
-		{complete_items = {'buffers'}},
-	},
-	string = {
-		{complete_items = {'path'}, triggered_only = {'./', '/'}},
-		{complete_items = {'buffers'}},
-	},
-	comment = {},
+  default = {
+    {complete_items = {'lsp', 'snippet'}},
+    {complete_items = {'path'}, triggered_only = {'./', '/'}},
+    {complete_items = {'buffers'}},
+  },
+  string = {
+    {complete_items = {'path'}, triggered_only = {'./', '/'}},
+    {complete_items = {'buffers'}},
+  },
+  comment = {},
 }
 
 local bytemarkers = { {0x7FF, 192}, {0xFFFF, 224}, {0x1FFFFF, 240} }
 local function utf8(decimal)
-	if decimal < 128 then
-		return string.char(decimal)
-	end
-	local charbytes = {}
-	for bytes,vals in ipairs(bytemarkers) do
-		if decimal <= vals[1] then
-			for b = bytes + 1, 2, -1 do
-				local mod = decimal % 64
-				decimal = (decimal - mod) / 64
-				charbytes[b] = string.char(128 + mod)
-			end
-			charbytes[1] = string.char(vals[2] + decimal)
-			break
-		end
-	end
-	return table.concat(charbytes)
+  if decimal < 128 then
+    return string.char(decimal)
+  end
+  local charbytes = {}
+  for bytes,vals in ipairs(bytemarkers) do
+    if decimal <= vals[1] then
+      for b = bytes + 1, 2, -1 do
+        local mod = decimal % 64
+        decimal = (decimal - mod) / 64
+        charbytes[b] = string.char(128 + mod)
+      end
+      charbytes[1] = string.char(vals[2] + decimal)
+      break
+    end
+  end
+  return table.concat(charbytes)
 end
 
 local customize_lsp_label = {
-	 Method = utf8(0xf794) .. ' [method]',
-	 Function = utf8(0xf794) .. ' [function]',
-	 Variable = utf8(0xf6a6) .. ' [variable]',
-	 Field = utf8(0xf6a6) .. ' [field]',
-	 Class = utf8(0xfb44) .. ' [class]',
-	 Struct = utf8(0xfb44) .. ' [struct]',
-	 Interface = utf8(0xf836) .. ' [interface]',
-	 Module = utf8(0xf668) .. ' [module]',
-	 Property = utf8(0xf0ad) .. ' [property]',
-	 Value = utf8(0xf77a) .. ' [value]',
-	 Enum = utf8(0xf77a) .. ' [enum]',
-	 Operator = utf8(0xf055) .. ' [operator]',
-	 Reference = utf8(0xf838) .. ' [reference]',
-	 Keyword = utf8(0xf80a) .. ' [keyword]',
-	 Color = utf8(0xe22b) .. ' [color]',
-	 Unit = utf8(0xe3ce) .. ' [unit]',
-	 Snippet = utf8(0xf68e) .. ' [snippet]',
-	 Text = utf8(0xf52b) .. ' [text]',
-	 Buffers = utf8(0xf64d) .. ' [buffers]',
-	 TypeParameter = utf8(0xf635) .. ' [type]',
+  Method = utf8(0xf794) .. ' [method]',
+  Function = utf8(0xf794) .. ' [function]',
+  Variable = utf8(0xf6a6) .. ' [variable]',
+  Field = utf8(0xf6a6) .. ' [field]',
+  Class = utf8(0xfb44) .. ' [class]',
+  Struct = utf8(0xfb44) .. ' [struct]',
+  Interface = utf8(0xf836) .. ' [interface]',
+  Module = utf8(0xf668) .. ' [module]',
+  Property = utf8(0xf0ad) .. ' [property]',
+  Value = utf8(0xf77a) .. ' [value]',
+  Enum = utf8(0xf77a) .. ' [enum]',
+  Operator = utf8(0xf055) .. ' [operator]',
+  Reference = utf8(0xf838) .. ' [reference]',
+  Keyword = utf8(0xf80a) .. ' [keyword]',
+  Color = utf8(0xe22b) .. ' [color]',
+  Unit = utf8(0xe3ce) .. ' [unit]',
+  Snippet = utf8(0xf68e) .. ' [snippet]',
+  Text = utf8(0xf52b) .. ' [text]',
+  Buffers = utf8(0xf64d) .. ' [buffers]',
+  TypeParameter = utf8(0xf635) .. ' [type]',
 }
 
 local on_attach = function(_, _)
-	require'diagnostic'.on_attach()
-	require'completion'.on_attach({
-		chain_complete_list = chain_complete_list,
-		customize_lsp_label = customize_lsp_label,
-		enable_auto_signatur = 1,
-		auto_change_source = 1,
-	})
-	vim.api.nvim_command('autocmd CursorHold <buffer> lua vim.lsp.util.show_line_diagnostics()')
-	vim.api.nvim_command('autocmd InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost <buffer> lua require"lsp_extensions".inlay_hints{ prefix = " » ", highlight = "NonText" }')
+  require'diagnostic'.on_attach()
+  require'completion'.on_attach({
+    chain_complete_list = chain_complete_list,
+    customize_lsp_label = customize_lsp_label,
+    enable_auto_signatur = 1,
+    auto_change_source = 1,
+  })
+  vim.api.nvim_command('autocmd CursorHold <buffer> lua vim.lsp.util.show_line_diagnostics()')
+  vim.api.nvim_command('autocmd InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost <buffer> lua require"lsp_extensions".inlay_hints{ prefix = " » ", highlight = "NonText" }')
 end
 
 local cap = {
-	textDocument = {
-		completion = {
-			completionItem = {
-				snippetSupport = true
-			}
-		}
-	}
+  textDocument = {
+    completion = {
+      completionItem = {
+        snippetSupport = true
+      }
+    }
+  }
 }
 
 local lua_settings = {
-	Lua = {
-		runtime = { version = "LuaJIT", path = vim.split(package.path, ';'), },
-		workspace = {
-			library = {
-				[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-			},
-		},
-	},
+  Lua = {
+    runtime = { version = "LuaJIT", path = vim.split(package.path, ';'), },
+    workspace = {
+      library = {
+        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+      },
+    },
+  },
 }
 
 local function setup_ls(ls, ls_cmd, backup, backup_cmd, passed_settings)
-	local bin, arr
-	local backup_bin, backup_arr
-	if type(ls_cmd) == "string" then
-		bin = ls_cmd
-		arr = { ls_cmd }
-	else
-		bin = ls_cmd[1]
-		arr = ls_cmd
-	end
+  local bin, arr
+  local backup_bin, backup_arr
+  if type(ls_cmd) == "string" then
+    bin = ls_cmd
+    arr = { ls_cmd }
+  else
+    bin = ls_cmd[1]
+    arr = ls_cmd
+  end
 
+  if not (backup_cmd == nil) then
+    if type(backup_cmd) == "string" then
+      backup_bin = backup_cmd
+      backup_arr = { backup_cmd }
+    else
+      backup_bin = backup_cmd[1]
+      backup_arr = backup_cmd
+    end
+  end
 
-	if not (backup_cmd == nil) then
-		if type(backup_cmd) == "string" then
-			backup_bin = backup_cmd
-			backup_arr = { backup_cmd }
-		else
-			backup_bin = backup_cmd[1]
-			backup_arr = backup_cmd
-		end
-	end
-
-	if util.has_bins(bin) then
-		ls.setup{
-			on_attach = on_attach;
-			cmd = arr;
-			capabilities = cap;
-			settings = passed_settings;
-		}
-	else
-		if not (backup == nil) then
-			if not (backup_bin == nil) and util.has_bins(backup_bin) then
-				backup.setup{
-					on_attach = on_attach;
-					cmd = backup_arr;
-					capabilities = cap;
-					settings = passed_settings;
-				}
-			end
-		end
-	end
+  if util.has_bins(bin) then
+    ls.setup{
+      on_attach = on_attach,
+      cmd = arr,
+      capabilities = cap,
+      settings = passed_settings,
+    }
+  else
+    if not (backup == nil) then
+      if not (backup_bin == nil) and util.has_bins(backup_bin) then
+        backup.setup{
+          on_attach = on_attach,
+          cmd = backup_arr,
+          capabilities = cap,
+          settings = passed_settings,
+        }
+      end
+    end
+  end
 end
 
 setup_ls(nvim_lsp.als, "ada_language_server")
--- .config/nvim/lua/module/treesitter.lua
+++ .config/nvim/lua/module/treesitter.lua
@@ -1,116 +1,116 @@
 local ts = require'nvim-treesitter.configs'
 
 ts.setup {
-	highlight = {
-		enable = true,
-	},
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			init_selection = 'gnn',
-			node_incremental = "grn",
-			scope_incremental = "grc",
-			node_decremental = "grm",
-		}
-	},
-	refactor = {
-		highlight_definitions = {
-			enable = true
-		},
-		highlight_current_scope = {
-			enable = false
-		},
-		smart_rename = {
-			enable = true,
-			keymaps = {
-				smart_rename = "grr",
-			}
-		},
-		navigation = {
-			enable = true,
-			keymaps = {
-				goto_definition = "gnd",
-				list_definitions = "gnD",
-				goto_next_usage = "<a-*>",
-				goto_previous_usage = "<a-#>",
-			}
-		}
-	},
-	textobjects = { -- syntax-aware textobjects
-		select = {
-			enable = true,
-			keymaps = {
-				["af"] = "@function.outer",
-				["if"] = "@function.inner",
-				["aC"] = "@class.outer",
-				["iC"] = "@class.inner",
-				["ac"] = "@conditional.outer",
-				["ic"] = "@conditional.inner",
-				["ae"] = "@block.outer",
-				["ie"] = "@block.inner",
-				["al"] = "@loop.outer",
-				["il"] = "@loop.inner",
-				["is"] = "@statement.inner",
-				["as"] = "@statement.outer",
-				["ad"] = "@comment.outer",
-				["am"] = "@call.outer",
-				["im"] = "@call.inner"
-			}
-		},
-		swap = {
-			enable = true,
-			swap_next = {
-				["<leader>a"] = "@parameter.inner",
-			},
-			swap_previous = {
-				["<leader>A"] = "@parameter.inner",
-			},
-		},
-		move = {
-			enable = true,
-			goto_next_start = {
-				["]m"] = "@function.outer",
-				["]]"] = "@class.outer",
-			},
-			goto_next_end = {
-				["]M"] = "@function.outer",
-				["]["] = "@class.outer",
-			},
-			goto_previous_start = {
-				["[m"] = "@function.outer",
-				["[["] = "@class.outer",
-			},
-			goto_previous_end = {
-				["[M"] = "@function.outer",
-				["[]"] = "@class.outer",
-			},
-		},
-	},
-	playground = {
-		enable = true,
-		updatetime = 25,
-		persist_queries = false
-	},
-	ensure_installed = { -- one of 'all', 'language' or a list of languages
-		'bash',
-		'c',
-		'cpp',
-		'css',
-		'c_sharp',
-		'go',
-		'html',
-		'java',
-		'javascript',
-		'json',
-		'lua',
-		'python',
-		'regex',
-		'rst',
-		'ruby',
-		'rust',
-		'scala',
-		'toml',
-		'typescript',
-		'yaml'
-	}
+  highlight = {
+    enable = true,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = 'gnn',
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    }
+  },
+  refactor = {
+    highlight_definitions = {
+      enable = true
+    },
+    highlight_current_scope = {
+      enable = false
+    },
+    smart_rename = {
+      enable = true,
+      keymaps = {
+        smart_rename = "grr",
+      }
+    },
+    navigation = {
+      enable = true,
+      keymaps = {
+        goto_definition = "gnd",
+        list_definitions = "gnD",
+        goto_next_usage = "<a-*>",
+        goto_previous_usage = "<a-#>",
+      }
+    }
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["aC"] = "@class.outer",
+        ["iC"] = "@class.inner",
+        ["ac"] = "@conditional.outer",
+        ["ic"] = "@conditional.inner",
+        ["ae"] = "@block.outer",
+        ["ie"] = "@block.inner",
+        ["al"] = "@loop.outer",
+        ["il"] = "@loop.inner",
+        ["is"] = "@statement.inner",
+        ["as"] = "@statement.outer",
+        ["ad"] = "@comment.outer",
+        ["am"] = "@call.outer",
+        ["im"] = "@call.inner"
+      }
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>a"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<leader>A"] = "@parameter.inner",
+      },
+    },
+    move = {
+      enable = true,
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+    },
+  },
+  playground = {
+    enable = true,
+    updatetime = 25,
+    persist_queries = false
+  },
+  ensure_installed = { -- one of 'all', 'language' or a list of languages
+    'bash',
+    'c',
+    'cpp',
+    'css',
+    'c_sharp',
+    'go',
+    'html',
+    'java',
+    'javascript',
+    'json',
+    'lua',
+    'python',
+    'regex',
+    'rst',
+    'ruby',
+    'rust',
+    'scala',
+    'toml',
+    'typescript',
+    'yaml'
+  }
 }
