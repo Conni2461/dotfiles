57d3a5b07f93a800ac4929fb5f343639dc008a24
Simon Hauser <Simon-Hauser@outlook.de>
2020-10-22 12:30:18 +0200

    Use fzy_sorter as generic and file sorter

--- .config/nvim/lua/module/telescope.lua
+++ .config/nvim/lua/module/telescope.lua
@@ -2,5 +2,7 @@ require('telescope').setup {
   defaults = {
     layout_strategy = "flex",
     winblend = 5,
+    generic_sorter = require'telescope.sorters'.get_fzy_sorter,
+    file_sorter = require'telescope.sorters'.get_fzy_sorter,
   }
 }
