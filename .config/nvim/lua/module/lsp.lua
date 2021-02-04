d6bc12d3d81e5d95b82ed9eaee4045e28d562b84
Simon Hauser <Simon-Hauser@outlook.de>
2021-02-04 15:50:28 +0100

    neovim: Align inlay_hints

--- .config/nvim/lua/module/lsp.lua
+++ .config/nvim/lua/module/lsp.lua
@@ -76,7 +76,7 @@ local on_attach = function(_, _)
   })
   vim.api.nvim_command('autocmd CursorHold <buffer> lua vim.lsp.diagnostic.show_line_diagnostics()')
   if vim.api.nvim_buf_get_option(0, 'filetype') == 'rust' then
-    vim.api.nvim_command('autocmd InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost <buffer> lua require"lsp_extensions".inlay_hints{ prefix = " » ", highlight = "NonText" }')
+    vim.api.nvim_command('autocmd InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost <buffer> lua require"lsp_extensions".inlay_hints{ prefix = " » ", highlight = "NonText", aligned = true, }')
   end
 end
 
