65834fa6033faf0c72c8e26d6e03181e39d99098
Simon Hauser <Simon-Hauser@outlook.de>
2021-04-22 11:31:24 +0200

    Make simpleline prettier
    Lsp and branch and some hlgroups for modes.
    I think its pretty decent now. Maybe i change some more hlgroups

--- .config/nvim/lua/module/simpleline.lua
+++ .config/nvim/lua/module/simpleline.lua
@@ -1,19 +1,64 @@
+local Job = require("plenary.job")
+
 local mode_map = {
-  ['n'] = 'NORMAL',
-  ['i'] = 'INSERT',
-  ['R'] = 'REPLACE',
-  ['v'] = 'VISUAL',
-  ['V'] = 'V-LINE',
-  ["\\<C-v>"]= 'V-BLOCK',
-  ['c'] = 'COMMAND',
-  ['s'] = 'SELECT',
-  ['S'] = 'S-LINE',
-  ["\\<C-s>"] = 'S-BLOCK',
-  ['t'] = 'TERMINAL',
+  ['n'] = { 'NORMAL', 'ColorColumn' },
+  ['i'] = { 'INSERT', 'Conceal' },
+  ['R'] = { 'REPLACE', 'Character' },
+  ['v'] = { 'VISUAL', 'Boolean' },
+  ['V'] = { 'V-LINE', 'Boolean' },
+  [""]= { 'V-BLOCK', 'Boolean' },
+  ['c'] = { 'COMMAND', 'ColorColumn' },
+  ['s'] = { 'SELECT', 'Boolean' },
+  ['S'] = { 'S-LINE', 'Boolean' },
+  [""] = { 'S-BLOCK', 'Boolean' },
+  ['t'] = { 'TERMINAL', 'Conceal' },
 }
 
-local block = function(hi, txt, new)
-  return string.format("%%#%s#%s%s", hi, txt, new and "%=" or "")
+local block = function(hi, txt)
+  return string.format("%%#%s#%s", hi, txt)
+end
+
+local static_entries = {
+  filename = block("ColorColumn", "%t"),
+  info = block("ColorColumn", "%l:%c | %p%%"),
+  inactive = block("ColorColumn", "%t"),
+  separator = block("ColorColumn", "%( | %)")
+}
+local cached_entires = { branch = "" }
+
+local if_left = function(left)
+  if left ~= "" then return static_entries.separator end
+  return ""
+end
+
+local severities = { "Error", "Warning", "Information", "Hint" }
+local severities_gens = {
+  "%%#LspDiagnosticsDefaultError#%%(" .. vim.g.indicator_errors .. "%d%%)",
+  "%%#LspDiagnosticsDefaultWarning#%%(" .. vim.g.indicator_warnings .. "%d%%)",
+  "%%#LspDiagnosticsDefaultInformation#%%(" .. vim.g.indicator_infos .. "%d%%)",
+  "%%#LspDiagnosticsDefaultHint#%%(" .. vim.g.indicator_hints .. "%d%%)",
+}
+local diagnostics = function()
+  local output = {}
+  local size = 0
+  for i = #severities, 1, -1 do
+    local res = vim.lsp.diagnostic.get_count(0, severities[i])
+    if res > 0 then
+      if size > 0 then
+        output[size] = output[size] .. " "
+      end
+      size = size + 1
+      output[size] = string.format(severities_gens[i], res)
+    end
+  end
+  return table.concat(output)
+end
+
+local branch = function()
+  if cached_entires.branch ~= "" then
+    return block("ColorColumn", string.format("%%( |  %s%%)", cached_entires.branch))
+  end
+  return ""
 end
 
 local m = {}
@@ -24,6 +69,12 @@ m.init = function()
 end
 
 m.update = function()
+  Job:new({ "git", "branch", "--show-current", cwd = vim.loop.cwd(), on_exit = function(j, c)
+    if c == 0 then
+      cached_entires.branch = j:result()[1]
+    end
+  end }):start()
+
   local curr = vim.fn.winnr()
   local last = vim.fn.winnr("$")
   for i = 1, last do
@@ -36,19 +87,25 @@ m.update = function()
 end
 
 m.active = function()
-  local mode = vim.api.nvim_get_mode().mode
+  local mode = mode_map[vim.fn.mode()]
+  local lsp = diagnostics()
+  local git = branch()
 
   local res = {
-    block("ColorColumn", string.format("%%( %s %%)", mode_map[mode]), false),
-    block("ColorColumn", "%t", true),
-    block("ColorColumn", "%p%%")
+    block(mode[2], string.format("%%( %s %%)", mode[1])),
+    static_entries.filename,
+    git,
+    "%=",
+    lsp,
+    if_left(lsp),
+    static_entries.info
   }
 
   return table.concat(res)
 end
 
 m.inactive = function()
-  return table.concat({ block("ColorColumn", "%t") })
+  return static_entries.inactive
 end
 
 return m
--- .config/nvim/plugins.d/60-nvim-lsp.vim
+++ .config/nvim/plugins.d/60-nvim-lsp.vim
@@ -3,10 +3,23 @@ let g:indicator_warnings = "\uf071 "
 let g:indicator_infos = "\uf7fc "
 let g:indicator_hints = "\ufbe6 "
 
-call sign_define("LspDiagnosticsSignError", {"text" : g:indicator_errors, "texthl" : "LspDiagnosticsDefaultError"})
-call sign_define("LspDiagnosticsSignWarning", {"text" : g:indicator_warnings, "texthl" : "LspDiagnosticsDefaultWarning"})
-call sign_define("LspDiagnosticsSignInformation", {"text" : g:indicator_infos, "texthl" : "LspDiagnosticsDefaultInformation"})
-call sign_define("LspDiagnosticsSignHint", {"text" : g:indicator_hints, "texthl" : "LspDiagnosticsDefaultHint"})
+hi LspDiagnosticsDefaultError guifg=Red guibg=#282a2e
+hi LspDiagnosticsDefaultWarning guifg=Orange guibg=#282a2e
+hi LspDiagnosticsDefaultInformation guifg=LightBlue guibg=#282a2e
+hi LspDiagnosticsDefaultHint guifg=LightGrey guibg=#282a2e
+
+call sign_define("LspDiagnosticsSignError", {
+			\ "text" : g:indicator_errors,
+			\ "texthl" : "LspDiagnosticsDefaultError"})
+call sign_define("LspDiagnosticsSignWarning", {
+			\ "text" : g:indicator_warnings,
+			\ "texthl" : "LspDiagnosticsDefaultWarning"})
+call sign_define("LspDiagnosticsSignInformation", {
+			\ "text" : g:indicator_infos,
+			\ "texthl" : "LspDiagnosticsDefaultInformation"})
+call sign_define("LspDiagnosticsSignHint", {
+			\ "text" : g:indicator_hints,
+			\ "texthl" : "LspDiagnosticsDefaultHint"})
 
 nnoremap <leader>af :w! \| !formatf <c-r>%<CR> \| :e <CR>
 
