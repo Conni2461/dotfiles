d3d6b5e66616a594a6611e344a5e304aed63a64c
Simon Hauser <simon.hauser@helsinki-systems.de>
2023-11-04 12:05:55 +0100

    chore: cleanup dwm autostart

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -1,24 +1,8 @@
 #!/bin/sh
 
-g810-led -a ff0000 &
 pgrep dunst >/dev/null || dunst &
-# pgrep compton >/dev/null || compton &
-fixdisplay &
-feh --bg-fill ~/img/2560x1440.jpg --bg-fill ~/img/1920x1080.jpg --no-fehbg
 pgrep clipmenud >/dev/null|| clipmenud &
-pgrep lxpolkit >/dev/null || lxpolkit &
 
-calsync >/dev/null &
-newsup >/dev/null &
-mailsync >/dev/null &
-
-dwmbar &
-pgrep conky >/dev/null || conky &
-
-pgrep calnotify.py >/dev/null || calnotify.py 30 &
-github-notify.py start
-
-pgrep nextcloud >/dev/null || nextcloud --background &
+ministatus 2>/tmp/ministatus.log &
 
 xset r rate 300 50
-setxkbmap -option caps:escape
