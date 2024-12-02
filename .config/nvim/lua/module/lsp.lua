958582190f385ede289ed0d86e2e9a9260c83514
Simon Hauser <Simon-Hauser@outlook.de>
2024-12-02 23:08:09 +0100

    fix: rust-analyzer for now

--- .config/nvim/lua/module/lsp.lua
+++ .config/nvim/lua/module/lsp.lua
@@ -222,14 +222,15 @@ for _, server in ipairs {
       },
     },
   },
-  { "nil_ls",
+  {
+    "nil_ls",
     settings = {
       ["nil"] = {
         formatting = {
-          command = {"nixfmt"},
-        }
-      }
-    }
+          command = { "nixfmt" },
+        },
+      },
+    },
   },
   {
     "pyright",
@@ -286,3 +287,13 @@ null_ls.setup {
     -- null_ls.builtins.diagnostics.luacheck
   },
 }
+
+for _, method in ipairs { "textDocument/diagnostic", "workspace/diagnostic" } do
+  local default_diagnostic_handler = vim.lsp.handlers[method]
+  vim.lsp.handlers[method] = function(err, result, context, config)
+    if err ~= nil and err.code == -32802 then
+      return
+    end
+    return default_diagnostic_handler(err, result, context, config)
+  end
+end
